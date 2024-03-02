32423d7c4d9ef22d0a01992d5e45dd6858c1fc8e
Kauan Pires <nauakpires@gmail.com>
2024-03-02 17:25:40 -0300

    First Commit

---
+++ src/ClubeDeFutebol.java
@@ -0,0 +1,33 @@
+public class ClubeDeFutebol {
+    private String nome;
+    private String cor;
+    private int idadeDoClube;
+
+    // Método para colocar o nome, cor e idade do clube.
+    public void setValores(String nome, String cor, int idadeDoClube) {
+        this.nome = nome;
+        this.cor = cor;
+        this.idadeDoClube = idadeDoClube;
+    }
+
+    // Método para pegar nome
+    public String getNome() {
+        return nome;
+    }
+
+    // Método para pegar cor
+    public String getCor() {
+        return cor;
+    }
+
+    // Método para pegar idade do clube
+    public int getIdadeDoClube() {
+        return idadeDoClube;
+    }
+
+    public void mostrarValores() {
+        System.out.println("Nome do Clube: " + nome);
+        System.out.println("Cor do clube: " + cor);
+        System.out.println("Idade do clube: " + idadeDoClube);
+    }
+}
---
+++ src/Principal.java
@@ -0,0 +1,7 @@
+public class Principal {
+    public static void main(String[] args) {
+        ClubeDeFutebol clube = new ClubeDeFutebol();
+        clube.setValores("Flamengo", "vermelho", 128);
+        clube.mostrarValores();
+    }
+}
